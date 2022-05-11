6fb6e3272d83c24d89ee539ca168d830a649b702
msanmaz <osanmaz4992@gmail.com>
2022-05-11 20:47:03 +0100

    heroku update

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
 
 
   useEffect(() => {
-    const socket = socketIOClient('http://localhost:3000');
+    const socket = socketIOClient('https://polar-taiga-09455.herokuapp.com/');
     socket.on("data", data => {
       setCrypto(data)
     });
