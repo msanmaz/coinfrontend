be1e4f6c2c631a5677b53bcd84d347c39ff00924
msanmaz <osanmaz4992@gmail.com>
2022-05-11 19:26:55 +0100

    little fixes

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,3 +1,47 @@
-import Swiper from "swiper";
+import {Swiper,SwiperSlide} from "swiper/react";
+import SwiperCore, { Navigation,Autoplay } from "swiper";
+import axios from "axios";
 
 
+// Import Swiper styles
+import 'swiper/css';
+import "swiper/css/navigation";
+SwiperCore.use([Navigation,Autoplay]);
+
+
+import React from 'react'
+import { Image,Box, Text, VStack } from "@chakra-ui/react";
+
+function Carousel({posts}) {
+
+    
+
+
+
+    
+  return (
+    <>
+    
+    <Box py={16} mx={20}>
+    <Swiper
+          spaceBetween={10}
+          slidesPerView={'auto'}
+          loop={true}
+          slidesPerGroup={5}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false
+        }}
+        >
+          {posts.coins.map(product => <SwiperSlide style={{width:'10%'}} key={product.name}><VStack> <Image src={product.iconUrl} w={'50%'}/> </VStack></SwiperSlide>)}
+
+
+        </Swiper>
+
+    </Box>
+
+    </>
+  )
+}
+
+export default Carousel
